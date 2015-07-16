b8aeecd72aab3fd7b37e6d5488e1b09ba9e8837b
ssthil <ssthil@gmail.com>
2015-07-16 11:07:13 +0800

    css modified

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.search-ul').hide();
+	//scroll
+	var stickyHeaderNav = ($('.search-container').offset().top+80);
+	$(window).scroll(function(){
+		if($(window).scrollTop() > stickyHeaderNav){
+			$('.search-ul').fadeIn();
+		} else {
+			$('.search-ul').fadeOut();
+		}
+	});
+});
